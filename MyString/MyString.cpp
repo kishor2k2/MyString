72e8182f5ff7480560982b4f928638ade41240b1
Kishor <kishor2k@gmail.com>
2024-07-31 01:55:17 +0530

    Added safe copy to ensure multiplatform support and safety against overflow

--- MyString/MyString.cpp
+++ MyString/MyString.cpp
@@ -3,15 +3,25 @@
 
 MyString::MyString() : data_(nullptr), length_(0) {}
 
+void safeCopy(char* dest, const char* src, size_t dest_size) {
+#ifdef _MSC_VER
+	strncpy_s(dest, dest_size, src, _TRUNCATE);
+#else
+	strncpy(dest, src, dest_size - 1);
+	dest[dest_size - 1] = '\0';
+#endif
+}
+
 MyString::MyString(const char* s) {
 	length_ = strlen(s);
 	data_ = new char[length_ + 1];
-	strcpy_s(data_, length_+1, s);
+	safeCopy(data_, s, length_ + 1);
 }
 
 MyString::MyString(const MyString& rhs) {
 	length_ = rhs.length_;
-	strcpy_s(data_, length_+1, rhs.data_);
+	data_ = new char[length_ + 1];
+	safeCopy(data_, rhs.data_, length_ + 1);
 }
 
 MyString::MyString(MyString&& rhs) noexcept : data_(rhs.data_), length_(rhs.length_) {
@@ -23,28 +33,12 @@ MyString::~MyString() {
 	delete[] data_;
 }
 
-size_t MyString::length() const noexcept {
-	return length_;
-}
-
-size_t MyString::size() const noexcept {
-	return length();
-}
-
-bool MyString::empty() const noexcept {
-	return length_ == 0;
-}
-
-const char* MyString::c_str() const noexcept {
-	return data_;
-}
-
 MyString& MyString::operator=(const MyString& rhs) {
 	if (this != &rhs) {
 		delete[] data_;
 		length_ = rhs.length_;
 		data_ = new char[length_ + 1];
-		strcpy_s(data_, rhs.length_+1, rhs.data_);
+		safeCopy(data_, rhs.data_, rhs.length_ + 1);
 	}
 	return *this;
 }
@@ -66,6 +60,22 @@ char& MyString::operator[](size_t index) {
 
 const char& MyString::operator[](size_t index) const {
 	return data_[index];
+}
+
+size_t MyString::length() const noexcept {
+	return length_;
+}
+
+size_t MyString::size() const noexcept {
+	return length();
+}
+
+bool MyString::empty() const noexcept {
+	return length_ == 0;
+}
+
+const char* MyString::c_str() const noexcept {
+	return data_;
 }
 
 std::ostream& operator<<(std::ostream& out, const MyString& str) {
